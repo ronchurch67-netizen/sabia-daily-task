3da27d2a1d86bb7f9d4df3e026dbb866137e33d7
ronchurch67-netizen <ronchurch67@gmail.com>
2026-08-15 15:52:49 -0700

    Fikse cache: HTML/CSS/JS toujou chèche dènye vèsyon an sou entènèt anvan

--- www/sw.js
+++ www/sw.js
@@ -1,4 +1,5 @@
-const CACHE_NAME = "tach-mwen-v2";
+const CACHE_NAME = "tach-mwen-v3";
+const CORE_ASSET_PATTERN = /\.(html|css|js|json)$/;
 const ASSETS_TO_CACHE = [
   "./",
   "./index.html",
@@ -27,7 +28,25 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    caches.match(event.request).then((cached) => cached || fetch(event.request))
-  );
+  const url = new URL(event.request.url);
+  const isCoreAsset = CORE_ASSET_PATTERN.test(url.pathname) || url.pathname.endsWith("/");
+
+  if (isCoreAsset) {
+    // Tanpri chèche nan entènèt anvan (pou toujou gen dènye vèsyon an),
+    // itilize kopi ki sove a sèlman si pa gen entènèt
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          const clone = response.clone();
+          caches.open(CACHE_NAME).then((cache) => cache.put(event.request, clone));
+          return response;
+        })
+        .catch(() => caches.match(event.request))
+    );
+  } else {
+    // Imaj ak lòt fichye ki pa chanje souvan: sèvi ak kopi a dabò, pi rapid
+    event.respondWith(
+      caches.match(event.request).then((cached) => cached || fetch(event.request))
+    );
+  }
 });
